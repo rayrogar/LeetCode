ff1c8a2d8a55b605444e02cb0fc0ceab012c3477
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-12 04:07:29 -0400

    creando el repo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace LeetCode
+{
+    public class Program{
+
+        public static void Main(string[] args){
+            //No_1480 a = new No_1480();
+            //No_1672 a = new No_1672();
+            //No_412 a = new No_412();
+            //No_1342 a = new No_1342();
+            //No_876 a = new No_876();
+            //No_383 a = new No_383();
+            No_71 a = new No_71();
+            //tema_arrays a = new tema_arrays();
+
+
+
+        }
+
+
+    }
+    
+}
