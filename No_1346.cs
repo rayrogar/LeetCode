9aa127208ffe2ee67110768484396c1cc121cbd0
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-12 15:21:42 -0400

    98ms Beats: 78.68%, 40.7MB Beats 69.67%

---
+++ No_1346.cs
@@ -0,0 +1,43 @@
+/*
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+ 
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+ 
+
+Constraints:
+
+2 <= arr.length <= 500
+-103 <= arr[i] <= 103
+*/
+
+namespace LeetCode
+{
+    public class No_1346{
+        public bool CheckIfExist(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j] * 2 || arr[i] * 2 == arr[j])
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
