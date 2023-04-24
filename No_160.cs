1bc6bf45b68fde5f52e5dd1e1ccc53efa62c1799
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-24 03:26:39 -0400

    Runtime: 2648 ms :muscle::muscle: Beats:5.4%:dizzy_face:, Memory: 54.5MB Beats: 5.57:dizzy_face:%

--- No_160.cs
+++ No_160.cs
@@ -32,46 +32,5 @@ namespace LeetCode
             }
             return null;
         }
-
-        public ListNode? GetIntersectionNode1(ListNode headA, ListNode headB)
-        {
-            List<ListNode> memA = new List<ListNode>();
-            List<ListNode> memB = new List<ListNode>();
-            ListNode a, a1, b, b1;
-            if (headA != null && headB != null)
-            {
-                a = headA;
-                a1 = headA.next;
-                b = headB;
-                b1 = headB.next;
-
-                while (a != null || b != null)
-                {
-                    if(a==b || a==b1 || memB.Contains(a))
-                        return a;
-                        else if(a1==b ||a1==b1 || memB.Contains(a1))
-                            return a1;
-                            else if(memA.Contains(b))
-                                return b;
-                                else if(memA.Contains(b1))
-                                    return b1;
-                                    else{
-                                            if (a != null)
-                                            {
-                                                a = a.next;
-                                                if (!memA.Contains(a)) 
-                                                    memA.Add(a); 
-                                            }
-
-                                            if (b != null){
-                                                b = b.next;
-                                                if (!memB.Contains(b)) 
-                                                memB.Add(b); 
-                                            }
-                                        }
-                }
-            }
-            return null;
-        }
     }
 }
