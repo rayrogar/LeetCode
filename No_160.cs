36705f500bc3f56518b79318ffd7a89378873e5b
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-25 11:38:19 -0400

    Runtime: 118ms Beats: 68.70%, Memory: 51.2MB Beats: 93.91%

--- No_160.cs
+++ No_160.cs
@@ -32,28 +32,46 @@ namespace LeetCode
             }
             return null;
         }
-
+        /// <summary>
+        /// RUNTIME: 1488ms BEATS: 5.4%, MEMORY: 52.3MB BEATS: 22.61%
+        /// </summary>
         public ListNode? GetIntersectionNode1(ListNode headA, ListNode headB)
         {
             List<ListNode> mem = new List<ListNode>();
             while (headA != null || headB != null)
             {
-                if (headA == headB)
-                    return headA;
-                if (mem.Contains(headA))
-                    return headA;
-                else if (headA != null)
+                while (headA != null)
                 {
                     mem.Add(headA);
                     headA = headA.next;
                 }
-                if (mem.Contains(headB))
-                    return headB;
-                else if (headB != null)
+                while (headB != null)
                 {
-                    mem.Add(headB);
+                    if (mem.Contains(headB))
+                    return headB;
                     headB = headB.next;
                 }
+            }
+            return null;
+        }
+        /// <summary>
+        /// RUNTIME: 118ms BEATS: 68.70%, MEMORY: 51.2MB BEATS: 93.91%
+        /// </summary>
+        public ListNode? GetIntersectionNode2(ListNode headA, ListNode headB)
+        {
+            HashSet<ListNode> mem = new HashSet<ListNode>();
+            while (headA != null)
+            {
+                mem.Add(headA);
+                headA = headA.next;
+
+            }
+
+            while (headB != null)
+            {
+                if (!mem.Add(headB))
+                    return headB;
+                headB = headB.next;
             }
             return null;
         }
