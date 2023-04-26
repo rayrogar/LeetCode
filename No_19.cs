f5b3c63cc762db5909800679fe8454c8f943fd39
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-26 16:45:01 -0400

    Runtime: 81 ms Beats: 81.18%, Memory: 39.1 mb Beats: 17.39%

---
+++ No_19.cs
@@ -0,0 +1,34 @@
+using System.Net.Http.Headers;
+
+namespace LeetCode
+{
+    public class No_19
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+
+            if (head.next == null)
+                return null;
+
+            int c = 1;
+            ListNode p = head;
+            while (p.next != null)
+            {
+                c++;
+                p = p.next;
+
+            }
+            if (c != n)
+            {
+                p = head;
+                for (int i = 1; i < c - n; i++)
+                    p = p.next;
+
+                p.next = p.next.next;
+                return head;
+            }
+            return head.next;
+
+        }
+    }
+}
