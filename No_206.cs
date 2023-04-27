7d2edcc60df180bc8f5ec2bcc9878ee41932adcc
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-26 22:35:26 -0400

    Runtime: 89ms Beats: 44.66%, Memory: 39.2mb Beats: 15.57%

---
+++ No_206.cs
@@ -0,0 +1,22 @@
+namespace LeetCode
+{
+    public class No_206
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head ==null || head.next == null)
+                return head;
+            ListNode p = new ListNode(int.MinValue), p1 = new ListNode(int.MinValue, head);
+
+            while (head.next != null)
+            {
+                p.next = head.next;
+                head.next = head.next.next;
+                p.next.next = p1.next;
+                p1.next = p.next;
+            }
+
+            return p.next;
+        }
+    }
+}
