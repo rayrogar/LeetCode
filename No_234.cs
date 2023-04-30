acb9a4ca66133dc59ca523faf7ea8a4eec09b14b
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-04-30 04:07:18 -0400

    Runtime: 260ms Beats: 21.39%, Memory: 62

---
+++ No_234.cs
@@ -0,0 +1,24 @@
+namespace LeetCode
+{
+    public class No_234
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            List<int> copy = new List<int>();
+
+            while (head != null)
+            {
+                copy.Add(head.val);
+                head = head.next;
+            }
+
+            for (int i = 0; i < copy.Count / 2; i++)
+            {
+                if (copy[i] != copy[copy.Count - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
