f51bcb3925af541f1a33581847c6d10449a49504
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-05-06 23:08:21 -0400

    Runtime: 168ms Beats: 51.92%, Memory: 44.5mb Beats: 35.15%

--- No_1.cs
+++ No_1.cs
@@ -1,8 +1,33 @@
+using System.Collections;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices.Marshalling;
+
 namespace LeetCode
 {
     public class No_1
     {
+        /// <summary>
+        /// Runtime: 168ms Beats: 51.92%, Memory: 44.5mb Beats: 28.84%
+        /// </summary>
         public int[] TwoSum(int[] nums, int target)
+        {
+            List<int> map = new List<int>();
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                var ans = target - nums[i];
+                if (map.Contains(ans))
+                    return new int[] { i, map.IndexOf(ans) };
+                else map.Add(nums[i]);
+            }
+            return new int[] { 0, 0 };
+
+        }
+
+        /// <summary>
+        /// Runtime: 175ms Beats: 42.21%, Memory: 44.5mb Beats: 35.15%
+        /// </summary>
+        public int[] TwoSum1(int[] nums, int target)
         {
             Dictionary<int, int> val_i = new Dictionary<int, int>();
             int max_less = nums[0];
