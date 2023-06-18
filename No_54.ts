38b8926be55b68108665bd1993387976af924fe7
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-06-18 03:30:49 -0400

    Runtime: 66ms Beats: 33.57%, Memory: 42.9mb Beats: 5053%

---
+++ No_54.ts
@@ -0,0 +1,43 @@
+namespace Leetcode{
+    export class No_54 {
+        spiralOrder(matrix: number[][]): number[] {
+
+            let up = 0, down = matrix.length, left = 0, rigth = matrix[0].length, nm = down * rigth, x = -1, y = 0;
+            let ans = [];
+  
+            while (nm > 0) {
+    
+                //move rigth  
+                while (x < rigth - 1 && nm > 0) {
+                    ans.push(matrix[y][++x]);
+                    nm--;
+                }
+                up++;
+    
+                //move down
+                while (y < down - 1 && nm > 0) {
+                    ans.push(matrix[++y][x]);
+                    nm--;
+                }
+                rigth--;
+    
+                //move left
+                while (x > left && nm > 0) {
+                    ans.push(matrix[y][--x]);
+                    nm--;
+                }
+                down--;
+    
+                //move up
+                while (y > up && nm > 0) {
+                    ans.push(matrix[--y][x]);
+                    nm--;
+                }
+    
+                left++;
+            }
+    
+            return ans;
+        };
+    }
+}
