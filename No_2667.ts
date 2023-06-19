705bde0b6470e19698e40d7c974c7542f0124a30
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-06-18 22:41:26 -0400

    Runtime: 64ms Beats: 38.65%, Memory: 42.8mb Beats: 49.89%

---
+++ No_2667.ts
@@ -0,0 +1,9 @@
+namespace Leetcode {
+    export class No_2667 {
+        createHelloWorld() {
+            return function (...args): string {
+                return "Hello World";       
+            };
+        };
+    }
+}
