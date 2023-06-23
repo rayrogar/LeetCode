a30cc6952b966b596aaae1cc0c84e9d3ce521d5f
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-06-22 21:41:46 -0400

    Runtime: 55ms Beats: 91.21%, Memory: 42.2mb Beats: 97.69%

--- No_2621.ts
+++ No_2621.ts
@@ -2,7 +2,7 @@ namespace Leetcode{
     export class No_2621 {
         async sleep(millis: number): Promise<void> {
             try {
-                return await new Promise<void>(resolve => setTimeout(() => resolve(), millis))
+                return new Promise<void>(resolve => setTimeout(() => resolve(), millis))
             }
             catch (error) { throw error; }
         }
