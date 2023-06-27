a9f25324415b68b231bf8ba11a1ce04222904abe
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-06-26 23:55:20 -0400

    Average Time of Process per Machine  Runtime: 442 ms, Beats: 70.97%

--- SQL_50.sql
+++ SQL_50.sql
@@ -39,3 +39,37 @@ left join employeeUNI on employeeUNI.id = employees.id*/
 select product.product_name, sales.year, sales.price from sales
 left join product on sales.product_id = product.product_id
 
+/* 1581. Customer Who Visited but Did Not Make Any Transactions Runtime: 2911ms Beats: 56.40% */
+
+select visits.customer_id, count(customer_id) as count_no_trans from visits
+where visit_id not in (select transactions.visit_id from transactions)
+group by visits.customer_id
+
+/* 197. Rising Temperature.  Runtime: 964ms Beats: 42.55%*/
+
+select weather.id from weather
+    join weather s on DATEDIFF(weather.recordDate,s.recordDate) = 1
+    where weather.temperature > s.temperature
+
+/* Average Time of Process per Machine  Runtime: 442 ms, Beats: 70.97% */
+
+select s.machine_id, round(avg(e.timestamp - s.timestamp),3) as processing_time
+from activity s
+join activity as e on s.machine_id = e.machine_id and s.activity_type = 'start' and e.activity_type = 'end'
+group by 1
+
+-- #1 - Acepted Runtime: 597 ms, Beats: 12.76%
+-- select machine_id, round(sum(if(activity_type = 'start', -1, 1) *timestamp)/count(distinct process_id),3) as processing_time
+-- from activity
+-- group by machine_id
+--
+-- #2 - Not Acepted, solo acercamiento
+-- select activity.machine_id, sum(activity.timestamp), count(distinct process_id)
+-- from activity
+-- group by activity.machine_id, activity.activity_type
+--
+-- #3 - Acepted Runtime: 482 ms, Beats: 49.54%
+-- select a.machine_id, round(avg(b.timestamp - a.timestamp), 3) as processing_time 
+-- from activity a 
+-- join activity b on a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start' and b.activity_type = 'end'
+-- group by 1;
