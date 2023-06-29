6c6a6561b8d36698014673496acd1023aec823e1
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-06-29 00:29:08 -0400

    Runtime: 68ms Beats: 41.5%, Memory: 45mb Beats: 30.88%

---
+++ No_1207.ts
@@ -0,0 +1,18 @@
+namespace Leetcode{
+    export class No_1207 {
+        uniqueOccurrences(arr: number[]): boolean {
+            let map = new Map<number, number>();
+
+            arr.forEach(x => { if (!map.has(x)) map.set(x, 1); else map.set(x, map.get(x) + 1); });
+            let sortedArray = [];
+            for (const x of map.values())
+                sortedArray.push(x);
+            sortedArray.sort();
+            let dif = Infinity;
+            let ans = true;
+
+            sortedArray.forEach(x => { if (x == dif) { ans = false; return; } dif = x; })
+            return ans;
+        };
+    }
+}
