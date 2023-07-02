6721e7e5bf32a7162ccc6c85dbb80f7e5d19b687
rayrogar <118454215+rayrogar@users.noreply.github.com>
2023-07-02 02:47:51 -0400

    Runtime: 65ms Beats: 76.67%, Memory: 44.8mb Beats: 64.87%

---
+++ No_746.ts
@@ -0,0 +1,9 @@
+namespace Leetcode{
+    export class No_746 {
+        minCostClimbingStairs(cost: number[]): number {
+            for (let i = 2; i < cost.length; i++)
+                cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+            return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+        };
+    }
+}
